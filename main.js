bbc88546f5fb60220cc4ce8989eda08f8499f219
Platon Brazhnikov <platon.brazhnikov@yandex.ru>
2022-06-05 21:28:40 +0300

    Сделал рандомную генерацию карточек из массива при загрузке страницы

--- main.js
+++ main.js
@@ -1,74 +1,393 @@
-import {ninja} from './ninja';
+const ninja = [
+    {
+        name: 'Naruto',
+        type: 'unknown',
+        hp: 564,
+        img: 'https://res.cloudinary.com/digaox5o1/image/upload/v1611014924/naruto_jrwlvd.png',
+        attacks: [
+            {
+                name: "Clone Lasso",
+                maxDamage: 40,
+                minDamage: 20,
+                maxCount: 100,
+            },
+            {
+                name: "Rasengan",
+                maxDamage: 60,
+                minDamage: 45,
+                maxCount: 6,
+            },
+            {
+                name: "Rasenshuriken",
+                maxDamage: 75,
+                minDamage: 60,
+                maxCount: 4,
+            },
+            {
+                name: "Twin Rasenshuriken",
+                maxDamage: 160,
+                minDamage: 130,
+                maxCount: 2,
+            }
+        ]
+    },
+    {
+        img: 'https://res.cloudinary.com/digaox5o1/image/upload/v1611014890/gaara_pnzqeo.png',
+        name: 'Gaara',
+        type: 'unknown',
+        hp: 282,
+        attacks: [
+            {
+                name: "Sand Shield",
+                maxDamage: 40,
+                minDamage: 20,
+                maxCount: 100,
+            },
+            {
+                name: "Sand Swords",
+                maxDamage: 60,
+                minDamage: 45,
+                maxCount: 6,
+            },
+            {
+                name: "Sand Shuriken",
+                maxDamage: 75,
+                minDamage: 60,
+                maxCount: 4,
+            },
+            {
+                name: "Sand Clone",
+                maxDamage: 130,
+                minDamage: 110,
+                maxCount: 2,
+            }
+        ]
+    },
+    {
+        img: 'https://res.cloudinary.com/digaox5o1/image/upload/v1611014890/ugito_jyfreh.png',
+        name: 'Yugito Nii',
+        type: 'unknown',
+        hp: 294,
+        attacks: [
+            {
+                name: "Fire Release",
+                maxDamage: 40,
+                minDamage: 20,
+                maxCount: 100,
+            },
+            {
+                name: "Mouse Attack",
+                maxDamage: 60,
+                minDamage: 45,
+                maxCount: 6,
+            },
+            {
+                name: "Attack Two-Tails",
+                maxDamage: 75,
+                minDamage: 60,
+                maxCount: 4,
+            },
+            {
+                name: "Massive Fireball",
+                maxDamage: 130,
+                minDamage: 110,
+                maxCount: 2,
+            }
+        ]
+    },
+    {
+        img: 'https://res.cloudinary.com/digaox5o1/image/upload/v1611014890/yagura_zzsd4e.png',
+        name: 'Yagura Karatachi',
+        type: 'unknown',
+        hp: 292,
+        attacks: [
+            {
+                name: "Water Release",
+                maxDamage: 40,
+                minDamage: 20,
+                maxCount: 100,
+            },
+            {
+                name: "Summoning Technique",
+                maxDamage: 60,
+                minDamage: 45,
+                maxCount: 6,
+            },
+            {
+                name: "Water Mirrors",
+                maxDamage: 75,
+                minDamage: 60,
+                maxCount: 4,
+            },
+            {
+                name: "Strong Gust",
+                maxDamage: 130,
+                minDamage: 110,
+                maxCount: 2,
+            }
+        ]
+    },
+    {
+        img: 'https://res.cloudinary.com/digaox5o1/image/upload/v1611014890/roshi_f0op22.png',
+        name: 'Rōshi',
+        type: 'unknown',
+        hp: 284,
+        attacks: [
+            {
+                name: "Earth Release",
+                maxDamage: 40,
+                minDamage: 20,
+                maxCount: 100,
+            },
+            {
+                name: "Kekkei Genkai",
+                maxDamage: 60,
+                minDamage: 45,
+                maxCount: 6,
+            },
+            {
+                name: "Lava Release",
+                maxDamage: 75,
+                minDamage: 60,
+                maxCount: 4,
+            },
+            {
+                name: "Tailed Beast Ball",
+                maxDamage: 130,
+                minDamage: 110,
+                maxCount: 2,
+            }
+        ]
+    },
+    {
+        img: 'https://res.cloudinary.com/digaox5o1/image/upload/v1611014890/khan_nfpqsh.png',
+        name: 'Han',
+        type: 'unknown',
+        hp: 404,
+        attacks: [
+            {
+                name: "Boil Release",
+                maxDamage: 40,
+                minDamage: 20,
+                maxCount: 100,
+            },
+            {
+                name: "Erupting Propulsion Fist",
+                maxDamage: 60,
+                minDamage: 45,
+                maxCount: 6,
+            },
+            {
+                name: "Five-Mountain Jumpe",
+                maxDamage: 75,
+                minDamage: 60,
+                maxCount: 4,
+            },
+            {
+                name: "Tailed Beast Ball",
+                maxDamage: 121,
+                minDamage: 97,
+                maxCount: 2,
+            }
+        ]
+    },
+    {
+        img: 'https://res.cloudinary.com/digaox5o1/image/upload/v1611014890/utakata_cx4e85.png',
+        name: 'Utakata',
+        type: 'unknown',
+        hp: 300,
+        attacks: [
+            {
+                name: "Soap Bubble Ninjutsu",
+                maxDamage: 40,
+                minDamage: 20,
+                maxCount: 100,
+            },
+            {
+                name: "Leech Gap",
+                maxDamage: 60,
+                minDamage: 45,
+                maxCount: 6,
+            },
+            {
+                name: "Wild Water Wave",
+                maxDamage: 75,
+                minDamage: 60,
+                maxCount: 4,
+            },
+            {
+                name: "Wisdom Wolf Decay",
+                maxDamage: 130,
+                minDamage: 110,
+                maxCount: 2,
+            }
+        ]
+    },
+    {
+        img: 'https://res.cloudinary.com/digaox5o1/image/upload/v1611014891/fuu_br2sjx.png',
+        name: 'Fū',
+        type: 'unknown',
+        hp: 280,
+        attacks: [
+            {
+                name: "Cocoon",
+                maxDamage: 40,
+                minDamage: 20,
+                maxCount: 100,
+            },
+            {
+                name: "Bug Bite",
+                maxDamage: 60,
+                minDamage: 45,
+                maxCount: 6,
+            },
+            {
+                name: "Scale Powder",
+                maxDamage: 75,
+                minDamage: 60,
+                maxCount: 4,
+            },
+            {
+                name: "Net-Shaped Prison",
+                maxDamage: 145,
+                minDamage: 115,
+                maxCount: 2,
+            }
+        ]
+    },
+    {
+        img: 'https://res.cloudinary.com/digaox5o1/image/upload/v1611014891/bi_gpzmra.png',
+        name: 'Killer B',
+        type: 'unknown',
+        hp: 385,
+        attacks: [
+            {
+                name: "Headbutt",
+                maxDamage: 40,
+                minDamage: 20,
+                maxCount: 100,
+            },
+            {
+                name: "Liger Bomb",
+                maxDamage: 60,
+                minDamage: 45,
+                maxCount: 6,
+            },
+            {
+                name: "Octopus Hug",
+                maxDamage: 75,
+                minDamage: 60,
+                maxCount: 4,
+            },
+            {
+                name: "Rising Bomber",
+                maxDamage: 125,
+                minDamage: 100,
+                maxCount: 1,
+            }
+        ]
+    },
+    {
+        img: 'https://res.cloudinary.com/digaox5o1/image/upload/v1611014891/khagomoro_drso04.png',
+        name: 'Madara Uchiha',
+        type: 'unknown',
+        hp: 390,
+        attacks: [
+            {
+                name: "Izanagi",
+                maxDamage: 40,
+                minDamage: 20,
+                maxCount: 100,
+            },
+            {
+                name: "Susanoo",
+                maxDamage: 60,
+                minDamage: 45,
+                maxCount: 6,
+            },
+            {
+                name: "Yasaka Magatama",
+                maxDamage: 75,
+                minDamage: 60,
+                maxCount: 4,
+            },
+            {
+                name: "Truth-Seeking Ball",
+                maxDamage: 180,
+                minDamage: 95,
+                maxCount: 1,
+            }
+        ]
+    }
+];
 
-console.log(ninja);
+// МАССИВ С ГЕРОЯМИ
+// МАССИВ С ГЕРОЯМИ
+// МАССИВ С ГЕРОЯМИ
+// МАССИВ С ГЕРОЯМИ
+// МАССИВ С ГЕРОЯМИ
+// МАССИВ С ГЕРОЯМИ
+// МАССИВ С ГЕРОЯМИ
+// МАССИВ С ГЕРОЯМИ
+// МАССИВ С ГЕРОЯМИ
+// МАССИВ С ГЕРОЯМИ
+// МАССИВ С ГЕРОЯМИ
+// МАССИВ С ГЕРОЯМИ
+// МАССИВ С ГЕРОЯМИ
+// МАССИВ С ГЕРОЯМИ
+// МАССИВ С ГЕРОЯМИ
 
-ninjaDataOne =  {
-  name: 'Naruto',
-  type: 'unknown',
-  hp: 564,
-  img: 'https://res.cloudinary.com/digaox5o1/image/upload/v1611014924/naruto_jrwlvd.png',
-  attacks: [
-      {
-          name: "Clone Lasso",
-          maxDamage: 40,
-          minDamage: 20,
-          maxCount: 100,
-      },
-      {
-          name: "Rasengan",
-          maxDamage: 60,
-          minDamage: 45,
-          maxCount: 6,
-      },
-      {
-          name: "Rasenshuriken",
-          maxDamage: 75,
-          minDamage: 60,
-          maxCount: 4,
-      },
-      {
-          name: "Twin Rasenshuriken",
-          maxDamage: 160,
-          minDamage: 130,
-          maxCount: 2,
-      }
-  ]
+function renderNormalHero(ninjaData) {
+    let normalHero = document.querySelector('#normal-card-template').content.cloneNode(true);
+    let normalHeroPhoto = normalHero.querySelector('.card__image');
+    normalHeroPhoto.src = ninjaData.img;
+    normalHeroPhoto.alt = ninjaData.name;
+    let normalHeroName = normalHero.querySelector('.card__name');
+    normalHeroName.textContent = ninjaData.name;
+    let normalHeroHP = normalHero.querySelector('#hero-hp');
+    normalHeroHP.textContent = ninjaData.hp;
+    let normalHeroAttack = normalHero.querySelector('#hero-attack');
+    normalHeroAttack.textContent = '100';
+    let normalHeroHPText = normalHero.querySelector('#health-character');
+    normalHeroHPText.textContent = `${ninjaData.hp} / ${ninjaData.hp}`;
+    return normalHero;
 }
 
-nanjaDataTwo = {
-  img: 'https://res.cloudinary.com/digaox5o1/image/upload/v1611014890/gaara_pnzqeo.png',
-  name: 'Gaara',
-  type: 'unknown',
-  hp: 282,
-  attacks: [
-      {
-          name: "Sand Shield",
-          maxDamage: 40,
-          minDamage: 20,
-          maxCount: 100,
-      },
-      {
-          name: "Sand Swords",
-          maxDamage: 60,
-          minDamage: 45,
-          maxCount: 6,
-      },
-      {
-          name: "Sand Shuriken",
-          maxDamage: 75,
-          minDamage: 60,
-          maxCount: 4,
-      },
-      {
-          name: "Sand Clone",
-          maxDamage: 130,
-          minDamage: 110,
-          maxCount: 2,
-      }
-  ]
+function insertNormalHeroToHTML(card) {
+    document.querySelector('.playground').prepend(card);
 }
 
-function renderHeroes() {
-    
+function renderDarkHero(ninjaData) {
+    let darkHero = document.querySelector('#dark-card-template').content.cloneNode(true);
+    let darkHeroPhoto = darkHero.querySelector('.card__image');
+    darkHeroPhoto.src = ninjaData.img;
+    darkHeroPhoto.alt = ninjaData.name;
+    let darkHeroName = darkHero.querySelector('.card__name');
+    darkHeroName.textContent = ninjaData.name;
+    let darkHeroHP = darkHero.querySelector('#hero-hp');
+    darkHeroHP.textContent = ninjaData.hp;
+    let darkHeroAttack = darkHero.querySelector('#hero-attack');
+    darkHeroAttack.textContent = '100';
+    let darkHeroHPText = darkHero.querySelector('#health-character');
+    darkHeroHPText.textContent = `${ninjaData.hp} / ${ninjaData.hp}`;
+    return darkHero;
 }
 
+function insertDarkHeroToHTML(card) {
+    document.querySelector('.playground').append(card);
+}
+
+let firstNinjaNumber = getRandomInt(0, ninja.length);
+let secontNinjaNumber = getRandomInt(0, ninja.length);
+while(firstNinjaNumber === secontNinjaNumber) {
+    secontNinjaNumber = getRandomInt(0, ninja.length);
+}
+
+insertNormalHeroToHTML(renderNormalHero(ninja[firstNinjaNumber]));
+insertDarkHeroToHTML(renderDarkHero(ninja[secontNinjaNumber]));
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //Максимум не включается, минимум включается
+}
